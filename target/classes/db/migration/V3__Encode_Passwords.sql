8655fa55ad2d05bff63871f9ae741b0f9820e088
Димас <52570598+GektoKot@users.noreply.github.com>
2020-12-08 15:18:58 +0100

    target remove

--- target/classes/db/migration/V3__Encode_Passwords.sql
+++
@@ -1,1 +0,0 @@
-update user set password='$2a$08$D1.HcE2OYWZPyPyOMByYheMlHhgnOY5/svW/DcnNCsdSXmvBnmmM.' where id=1;
--- target/classes/db/migration/V4__Add_subscriptions_table.sql
+++
@@ -1,6 +0,0 @@
-create table user_subscriptions
-(
-    channel_id int not null references user,
-    subscriber_id int not null references user,
-    primary key (channel_id, subscriber_id)
-)
